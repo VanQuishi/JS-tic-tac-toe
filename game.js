d81d723906a49f6b955047d707382bcaf19201b1
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2021-11-15 22:03:00 -0600

    write 2 methods for gameboard

--- game.js
+++ game.js
@@ -1,3 +1,53 @@
-const gameBoard = (() => {
+const GameBoard = (() => {
   //store gameboard as an array in here
+  var boardArray = [
+    ['x', 'x', 'x'],
+    ['o', 'x', 'o'],
+    ['o', 'o', 'x']
+  ]
+
+  // function displayBoard() {
+  //   //console.log({boardArray});
+  //   boardArray.map( function(row, rowIndex) {
+  //     //console.log({row});
+  //     row.map( function(cellMark, cellIndex) {
+  //       //console.log({cellMark});
+  //       var cell = document.getElementById('c' + rowIndex + cellIndex);
+  //       cell.innerHTML = cellMark;
+  //     });
+  //   });
+  // }
+
+  //diffrent syntax that also works
+  const displayBoard = () => {
+    boardArray.map((row, rowIndex) => {
+      row.map((cellMark, cellIndex) => {
+        var cell = document.getElementById('c' + rowIndex + cellIndex);
+        cell.innerHTML = cellMark;
+      });
+    });
+  }
+
+  const writeToBoard = (x, y, mark) => {
+    boardArray[x][y] = mark;
+  }
+  //const gameOver = () =>
+
+  return {
+    displayBoard,
+    writeToBoard
+  };
 })();
+
+const Player = (playerName, playerMark) => {
+  return {playerName, playerMark};
+};
+
+const DisplayController = (() => {
+  //const assignPlayerMark = (playerName, playerMark) =>
+  //const announceWinner = () =>
+})();
+
+GameBoard.displayBoard();
+GameBoard.writeToBoard(0,0,'o');
+GameBoard.displayBoard();
