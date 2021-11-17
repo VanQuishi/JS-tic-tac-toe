41c48657686bab0593c223cb185133c32d500fba
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2021-11-16 19:50:33 -0600

    finish initial methods for GameBoard

--- game.js
+++ game.js
@@ -7,11 +7,8 @@ const GameBoard = (() => {
   ]
 
   // function displayBoard() {
-  //   //console.log({boardArray});
   //   boardArray.map( function(row, rowIndex) {
-  //     //console.log({row});
   //     row.map( function(cellMark, cellIndex) {
-  //       //console.log({cellMark});
   //       var cell = document.getElementById('c' + rowIndex + cellIndex);
   //       cell.innerHTML = cellMark;
   //     });
@@ -31,11 +28,47 @@ const GameBoard = (() => {
   const writeToBoard = (x, y, mark) => {
     boardArray[x][y] = mark;
   }
-  //const gameOver = () =>
+
+  const gameOver = () => {
+
+    for (let i = 0; i < 3; i++) {
+      if (boardArray[i][0] != '') {
+        if (boardArray[i][0] == boardArray[i][1] && boardArray[i][0] == boardArray[i][2]) {
+          console.log('game over, row match!');
+          return;
+        }
+      }
+
+      if (boardArray[0][i] != '') {
+        if (boardArray[0][i] == boardArray[1][i] && boardArray[0][i] == boardArray[2][i]) {
+          console.log('game over, column match!');
+          return;
+        }
+      }
+    }
+
+    //no match is found. check if the board is filled => draw
+    var filledBoardFlag = true;
+
+    for (let row = 0; row < 3; row++) {
+      if (boardArray[row][0] == '' || boardArray[row][1] == '' || boardArray[row][2] == '') {
+        filledBoardFlag = false;
+        break;
+      }
+    }
+
+    if (filledBoardFlag) {
+      console.log('draw!');
+    } else {
+      console.log('game can continue');
+    }
+
+  }
 
   return {
     displayBoard,
-    writeToBoard
+    writeToBoard,
+    gameOver
   };
 })();
 
@@ -48,6 +81,8 @@ const DisplayController = (() => {
   //const announceWinner = () =>
 })();
 
+// GameBoard.displayBoard();
+GameBoard.writeToBoard(0,0,'x');
+GameBoard.writeToBoard(1,0,'o');
 GameBoard.displayBoard();
-GameBoard.writeToBoard(0,0,'o');
-GameBoard.displayBoard();
+GameBoard.gameOver();
